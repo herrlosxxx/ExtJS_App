f7c8637b704cc55092f4059a87405101faad1d4c
Andrew <spylogger@mail.ru>
2017-01-23 01:19:03 +0300

    Убрал лишний код, который не используется в текущей реализации задачи

--- source/app/view/main/Tree.js
+++ source/app/view/main/Tree.js
@@ -15,36 +15,6 @@ Ext.define('UIS.view.main.Tree', {
             ddGroup: 'uisDD',
             enableDrag: true,
             enableDrop: false
-        },
-        listeners: {
-            drag: function() {
-                console.log('draggin');
-            },
-            beforedrop: function(node, data) {
-                data.records[0].set('leaf', true);
-                data.records[0].set('checked', null);
-                console && console.log(arguments);
-            },
-            drop: function(node, data, dropRec, dropPosition) {
-                var dropOn = dropRec ? ' ' + dropPosition + ' ' + dropRec.get('text') : ' on empty view';
-                Ext.example.msg("Drag from left to right", 'Dropped ' + data.records[0].get('text')  + dropOn);
-                firstGrid.store.remove(data.records[0]);
-            }
         }
-    },
-    listeners: {
-        drag: function() {
-                console.log('draggin');
-            },
-            beforedrop: function(node, data) {
-                data.records[0].set('leaf', true);
-                data.records[0].set('checked', null);
-                console && console.log(arguments);
-            },
-            drop: function(node, data, dropRec, dropPosition) {
-                var dropOn = dropRec ? ' ' + dropPosition + ' ' + dropRec.get('text') : ' on empty view';
-                Ext.example.msg("Drag from left to right", 'Dropped ' + data.records[0].get('text')  + dropOn);
-                firstGrid.store.remove(data.records[0]);
-            }
     }
 });
